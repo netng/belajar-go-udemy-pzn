0a08c9ce82df35d7a68127155e464c4559d4de3f
netng <net.nandang@gmail.com>
2022-06-22 13:33:38 +0700

    string

--- string.go
+++ string.go
@@ -5,4 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Ini adalah string")
 	fmt.Println("Diapit dengan tandan petik dua")
+
+	fmt.Println("Jumlah karakter dari 'kasur' adalah: ", len("kasur"))
+	fmt.Println("Karakter pertama dari string 'sayur' adalah: ", "sayur"[0])
 }
