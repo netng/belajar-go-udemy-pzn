c611d8136e4a0f065262d3bb331c31d9bfdfd9ec
netng <net.nandang@gmail.com>
2022-06-22 13:45:14 +0700

    string

---
+++ variable.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "ucup programmer"
+	fmt.Println("Hello, ", name)
+
+	name = "udin programmer"
+	fmt.Println("Hello, ", name)
+
+	var friendName = "ujang"
+	fmt.Println(friendName)
+
+	fatherName := "aep"
+	fmt.Println(fatherName)
+
+	age := 60
+	fmt.Println(age)
+}
