1ec4a1dd6bfb19902b213c218df0fe0bea6f5f3b
netng <net.nandang@gmail.com>
2022-06-23 10:43:45 +0700

    math operation

---
+++ math-operation.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	result := 10 + 10
+	fmt.Println(result)
+
+	a := 10
+	b := 10
+	c := a * b
+	fmt.Println(c)
+
+	i := 10
+	i += a
+	fmt.Println(i)
+
+	i++
+	fmt.Println(i)
+
+	negative := -10
+	fmt.Println(negative)
+}
